699c0aff558dc61c17915b6482047f01e8668f64
OmarHamdann <omar.haamdan@gmail.com>
2026-08-02 17:56:39 +0300

    test error

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
 const start = async () => {
   await app.listen(PORT)
   console.log(`server started on port ${PORT}`)
